a00a65b77da9c9cd93bd385d9904df4eeee6f44f
Gustavo Souza <gustavo.souza.1449179@sga.pucminas.br>
2023-06-22 14:09:54 -0300

    add

---
+++ src/Js/perfil.js
@@ -0,0 +1,5 @@
+
+const userEmail = localStorage.getItem('userEmail');
+const userSenha = localStorage.getItem('userSenha');
+const userNome  =  localStorage.getItem('userSenha');
+
--- src/Js/relatorio.js
+++ src/Js/relatorio.js
@@ -1,57 +1,56 @@
 let arrayMed = JSON.parse(localStorage.getItem('arrayMed'));
 
-function rel () {
-// Obtém uma referência para a tabela
-let table = document.querySelector('table');
+function rel() {
 
-for (let i = 0; i < arrayMed.length; i++) {
-  // Cria uma nova linha (<tr>) para cada elemento do array
-  let row = document.createElement('tr');
+    let table = document.querySelector('table');
 
-  // Cria uma célula (<td>) para cada propriedade do objeto
-  let medicoCell = document.createElement('td');
-  let nomeCell = document.createElement('td');
-  let dosagemCell = document.createElement('td');
-  let horarioCell = document.createElement('td');
-  let confirmadoCell = document.createElement('td');
+    for (let i = 0; i < arrayMed.length; i++) {
 
-  // Adiciona o conteúdo do array nas células
-  medicoCell.textContent = arrayMed[i].medico;
-  nomeCell.textContent = arrayMed[i].nome;
-  dosagemCell.textContent = arrayMed[i].dosagem;
-  horarioCell.textContent = arrayMed[i].horario;
-  confirmadoCell.textContent = arrayMed[i].confirmado;
+        let row = document.createElement('tr');
 
-  // Adiciona as células à linha
-  row.appendChild(medicoCell);
-  row.appendChild(nomeCell);
-  row.appendChild(dosagemCell);
-  row.appendChild(horarioCell);
-  row.appendChild(confirmadoCell);
 
-  // Adiciona a linha à tabela
-  table.appendChild(row);
+        let medicoCell = document.createElement('td');
+        let nomeCell = document.createElement('td');
+        let dosagemCell = document.createElement('td');
+        let horarioCell = document.createElement('td');
+        let confirmadoCell = document.createElement('td');
+
+
+        medicoCell.textContent = arrayMed[i].medico;
+        nomeCell.textContent = arrayMed[i].nome;
+        dosagemCell.textContent = arrayMed[i].dosagem;
+        horarioCell.textContent = arrayMed[i].horario;
+        confirmadoCell.textContent = arrayMed[i].confirmado;
+
+
+        row.appendChild(medicoCell);
+        row.appendChild(nomeCell);
+        row.appendChild(dosagemCell);
+        row.appendChild(horarioCell);
+        row.appendChild(confirmadoCell);
+
+
+        table.appendChild(row);
+    }
+
 }
 
-}
 
-// Código para o botão "Gerar"
 let gerarButton = document.querySelector('.gerar-button');
-gerarButton.addEventListener('click', function() {
-  // Lógica para criar a tabela
+gerarButton.addEventListener('click', function () {
 
-  // Mostrar o botão "Compartilhar"
-  let compartilharButton = document.querySelector('#compartilhar-button');
-  compartilharButton.style.display = 'block';
+
+    let compartilharButton = document.querySelector('#compartilhar-button');
+    compartilharButton.style.display = 'block';
 });
 
-// Código para o botão "Compartilhar"
+
 let compartilharButton = document.querySelector('#compartilhar-button');
-compartilharButton.addEventListener('click', function() {
-  // Obter os dados da tabela
+compartilharButton.addEventListener('click', function () {
 
-  // Obter o email do usuário do localStorage
-  let userEmail = localStorage.getItem('userEmail');
 
-  // Lógica para enviar os dados da tabela por e-mail usando a variável "userEmail"
+
+    let userEmail = localStorage.getItem('userEmail');
+
+
 });
